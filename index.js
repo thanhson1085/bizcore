ab11ce7fd89c08d7bf6d4801727de91e8c2643ca
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-03-12 13:48:10 +0000

    swagger ui

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
 var express = require('express');
+var yaml = require('js-yaml');
 var ParseServer = require('parse-server').ParseServer;
+var fs = require('fs');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
@@ -27,10 +26,17 @@ var app = express();
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-    res.status(200).send('I dream of being a web site.');
+// api-json swagger
+app.get('/docs', function(req, res){
+    var docs = yaml.safeLoad(fs.readFileSync('./docs/swagger.yml', 'utf8'));
+    res.send(JSON.stringify(docs));
 });
+
+// add-on swagger-ui
+app.use('/swagger', express.static('./node_modules/swagger-ui/dist'));
+
+// redirect page
+app.use('/', express.static('./docs'));
 
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
