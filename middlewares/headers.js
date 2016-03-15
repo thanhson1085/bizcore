d4a6c7f63be47f1e051eb7b47f353a3c8e247efd
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-03-15 04:04:41 +0000

    change parse header

--- middlewares/headers.js
+++ middlewares/headers.js
@@ -1,11 +1,13 @@
 'use strict';
 module.exports = function(req, res, next) {
+
+    // alow X-Koor-Application-Id
     res.header('X-Koor-Application-Id');
-    console.log(req.get('X-Koor-Application-Id'));
+
     var koorApplicationId = req.get('X-Koor-Application-Id');
 
     if (koorApplicationId) {
-        res.setHeader('X-Parse-Application-Id', koorApplicationId);
+        req.headers['x-parse-application-id'] = koorApplicationId;
     }
     next();
 };
