9fee7a42a2ef9fa751585bdc913963bc7d076cf6
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-03-23 11:16:42 +0000

    remove export swagger

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var ParseServer = require('parse-server').ParseServer;
 var logger = require('./helpers/logger');
 var express = require('express');
-var yaml = require('js-yaml');
 var config = require('config');
 
 module.exports = {
@@ -18,17 +17,6 @@ module.exports = {
         var app = express();
         app.use(require('./middlewares/headers'));
 
-        // api-json swagger
-        app.get('/docs', function(req, res){
-            var docs = yaml.safeLoad(fs.readFileSync('./docs/swagger.yml', 'utf8'));
-            res.send(JSON.stringify(docs));
-        });
-
-        // add-on swagger-ui
-        app.use('/swagger', express.static('./node_modules/swagger-editor/'));
-
-        // redirect page
-        app.use('/', express.static('./docs'));
         return app;
     }
 };
