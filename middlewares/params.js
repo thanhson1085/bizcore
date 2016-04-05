72ddaa9de4e7f96740efdaad5b7f3658770e7e35
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-05 10:51:11 +0000

    tmp remove params

--- middlewares/params.js
+++ middlewares/params.js
@@ -1,10 +1,12 @@
 'use strict';
 module.exports = function(req, res, next) {
+    /*
     if (req.method === 'GET') {
         for (var key in req.query) {
             req.query['where'] = '{ "' + key + '": "' + req.query[key] + '" }';
             delete req.query[key];
         }
     }
+    */
     next();
 };
